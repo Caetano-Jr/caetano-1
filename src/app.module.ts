14a56d49c9d65d0f578d2fd0ec911e1fce542aba
caetano santos <caetanojunior017@gmail.com>
2024-10-22 16:22:04 -0100

    2nd commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { BarbershopModule } from './barbershop/barbershop.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, UserModule, BarbershopModule],
