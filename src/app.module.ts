9ab77e87a9f164b93fef1a67cca085696fa20fda
caetano santos <caetanojunior017@gmail.com>
2024-10-23 16:54:12 -0100

    logica de autenticacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+
 import { BarbershopModule } from './barbershop/barbershop.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BarbershopModule],
+  imports: [AuthModule, UserModule, BarbershopModule, PrismaModule],
+  controllers:[AuthController],
+  providers:[AuthService]
 })
 export class AppModule {}
